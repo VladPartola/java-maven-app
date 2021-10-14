d6b8f56bba851656b448cd5886a977f16cc60d19
VladPartola <74604856+VladPartola@users.noreply.github.com>
2021-10-14 14:02:30 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,17 +1,3 @@
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
-
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-key', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh 'docker build -t vladpartola/java-maven-app:jma-2.0 .'
-        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh 'docker push vladpartola/java-maven-app:jma-2.0'
-    }
-} 
-
 def deployApp() {
     echo 'deploying the application...'
 } 
