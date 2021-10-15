0c28078b4aca971df11e03a79a114aef9f3d1946
VladPartola <74604856+VladPartola@users.noreply.github.com>
2021-10-15 20:16:42 +0300

    Merge to one command

--- script.groovy
+++ script.groovy
@@ -16,9 +16,7 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-key', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh "docker build -t vladpartola/java-maven-app:test_$IMAGE_NAME ."
-        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh 'docker push vladpartola/java-maven-app:$IMAGE_NAME'
+        sh "docker build -t vladpartola/java-maven-app:test_$IMAGE_NAME . | echo $PASSWORD | docker login -u $USERNAME --password-stdin | docker push vladpartola/java-maven-app:$IMAGE_NAME"
     }
 } 
 
