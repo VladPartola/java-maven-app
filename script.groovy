0064f813bf63da4a524b25d90c5a8af3cba557e3
VladPartola <74604856+VladPartola@users.noreply.github.com>
2021-10-15 20:13:31 +0300

    Add def incrementalVersion()

--- script.groovy
+++ script.groovy
@@ -1,3 +1,13 @@
+def incrementalVersion() {
+     echo 'incrementing app version...'
+     sh "mvn build-helper:parse-version versions:set \
+         -DnewVersion=\\\${parsedVersion:majorVersion}.\\\${parsedVersion:minorVersion}.\\\${parsedVersion:nextIncrementalVersion} \
+         versions:commit"
+     def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
+     def version = matcher[0][1]
+     env.IMAGE_NAME = "$version-$BUILD_NUMBER"
+}
+
 def buildJar() {
     echo "building the application..."
     sh 'mvn clean package'
@@ -8,7 +18,7 @@ def buildImage() {
     withCredentials([usernamePassword(credentialsId: 'docker-key', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
         sh "docker build -t vladpartola/java-maven-app:test_$IMAGE_NAME ."
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh 'docker push vladpartola/java-maven-app:test'
+        sh 'docker push vladpartola/java-maven-app:$IMAGE_NAME'
     }
 } 
 
