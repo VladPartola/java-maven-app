d1eb83e47f189cfa0128c3f1910da70be1130cb2
VladPartola <74604856+VladPartola@users.noreply.github.com>
2021-10-15 20:31:34 +0300

    Fix variables error

--- script.groovy
+++ script.groovy
@@ -16,9 +16,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-key', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh "docker login -u vladpartola -p DOCKERjeffhardy1997"
-        sh "docker build -t vladpartola/java-maven-app:test_$IMAGE_NAME ." 
-        sh "docker push vladpartola/java-maven-app:$IMAGE_NAME"  
+         sh "docker build -t vladpartola/java-maven-app:${IMAGE_NAME} ."
+         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
+         sh "docker push vladpartola/java-maven-app:${IMAGE_NAME}"  
     }
 } 
 
